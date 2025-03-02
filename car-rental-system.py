291c2ae8465e5c00b9cdfd491e739d0ffe9a2603
Hussein <husseinmayta@gmail.com>
2025-03-02 20:33:28 +0200

    Create method to display bike engine capacity

--- car-rental-system.py
+++ car-rental-system.py
@@ -32,3 +32,7 @@ class Bike(Vehicle):
     def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
         self.engine_capacity = engine_capacity 
+    
+    def display_info(self):
+        print(f"\nBike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price_per_day()}/day")
+    
