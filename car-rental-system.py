fc043c988fc076baf9d12973f89ffba39eef7a7d
Hussein <husseinmayta@gmail.com>
2025-03-02 20:20:21 +0200

    Add init method

--- car-rental-system.py
+++ car-rental-system.py
@@ -1,2 +1,6 @@
 class Vehicle:
-    
+    def __init__(self, brand, model, year, rental_price_per_day):
+        self.brand = brand  
+        self.model = model  
+        self.year = year  
+        self.__rental_price_per_day = rental_price_per_day  # Private attribute
