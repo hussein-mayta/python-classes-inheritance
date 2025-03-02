7a079a7c735333842a1eefa90881efe7bdcc4a60
Hussein <husseinmayta@gmail.com>
2025-03-02 20:22:42 +0200

    Add method to print informations

--- car-rental-system.py
+++ car-rental-system.py
@@ -4,3 +4,6 @@ class Vehicle:
         self.model = model  
         self.year = year  
         self.__rental_price_per_day = rental_price_per_day  # Private attribute
+
+    def display_info(self):
+        print(f"{self.brand} {self.model}, Year: {self.year}, Rental Price: ${self.__rental_price_per_day}/day")
